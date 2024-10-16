cf27fb80bde0d6d259179b79185bb3b97173b43a
Istiak Ahmed <istiakahmed6219@gmail.com>
2024-10-16 20:35:44 +0600

    create reuseable component

---
+++ src/utils/link.js
@@ -0,0 +1,46 @@
+export const examsDropdownLinks = [
+  {
+    text: "NEET",
+    to: "/neet",
+    nestedLinks: [
+      { text: "Class 11th", to: "/neet/online-coaching-class-11" },
+      { text: "Class 12th", to: "/neet/online-coaching-class-11" },
+      { text: "Class 12th Plus", to: "" },
+    ],
+  },
+  {
+    text: "JEE",
+    to: "/jee",
+    nestedLinks: [
+      { text: "Class 11th", to: "/neet/online-coaching-class-11" },
+      { text: "Class 12th", to: "/neet/online-coaching-class-11" },
+      { text: "Class 12th Plus", to: "" },
+    ],
+  },
+  {
+    text: "Class 6-10",
+    to: "/class-6-10",
+    nestedLinks: [
+      { text: "Class 6", to: "" },
+      { text: "class 7", to: "" },
+      { text: "class 8", to: "" },
+      { text: "class 9", to: "" },
+      { text: "class 10", to: "" },
+    ],
+  },
+];
+
+export const programsDropdownLinks = [
+  {
+    text: "Online Programs",
+    to: "/online-programs",
+  },
+  {
+    text: "Classrooms Programs",
+    to: "/classroom-programs",
+  },
+  {
+    text: "Distance Learning",
+    to: "/distance-learning",
+  },
+];
